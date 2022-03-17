0982a4798f74de8be32bc4955a08a79cc89f009d
KruyerL <kruyerl@gmail.com>
2022-03-17 18:23:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* 
+Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+For example, a tower with 3 floors looks like this:
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+*/
+
+const towerBuilder = (nFloors) => {};
+
+towerBuilder(5);
